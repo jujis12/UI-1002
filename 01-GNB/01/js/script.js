78304b2393658427882478a6a6f3636d4d129e7b
jujis12 <tjrdn0815@naver.com>
2024-10-02 13:43:31 +0900

    기본 동작 구현

--- 01-GNB/01/js/script.js
+++ 01-GNB/01/js/script.js
@@ -1,7 +1,7 @@
 $(function () {
     // 대상을 변수에 저장
     const $menu = $(".gnb > li");
-    const $sub = $(".submenu");
+    const $submenu = $(".submenu");
 
     // 메뉴에 마우스가 들어왔을 때
     $menu.on("mouseenter", function () {
